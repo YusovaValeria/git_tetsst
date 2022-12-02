2a1e38cd15c0c09caf0b46eb6b1126d85472b1fd
YusovaValeria <ler04ka02@mail.ru>
2022-12-03 02:07:46 +0700

    основа

--- proga/Program.cs
+++ proga/Program.cs
@@ -6,7 +6,33 @@ namespace proga
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] menuItems = new string[] { "Введите A", "Введите B", "+", "-", "*", "/", "End", };
+
+            Console.WriteLine("Меню");
+            Console.WriteLine();
+
+            int row = Console.CursorTop;
+            int col = Console.CursorLeft;
+            while (true)
+            {
+               
+            }
+        }
+
+        private static void DrawMenu(string[] items, int row, int col, int index, int result)
+        {
+            Console.SetCursorPosition(col, row);
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (i == index)
+                {
+                    Console.BackgroundColor = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                Console.WriteLine(items[i]);
+                Console.ResetColor();
+            }
+            Console.WriteLine();
         }
     }
 }
