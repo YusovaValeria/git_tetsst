cf5f1d87c8c4191c97d9fa1ce7bad512dc257454
YusovaValeria <ler04ka02@mail.ru>
2022-12-03 03:01:16 +0700

    +

--- proga/Program.cs
+++ proga/Program.cs
@@ -35,16 +35,10 @@ namespace proga
                     case ConsoleKey.Enter:
                         switch (index)
                         {
-                            case 0:
-                                Console.WriteLine("Введите А:");
-                                A = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine(A);
-                                break;
 
-                            case 1:
-                                Console.Write("Введите B: ");
-                                B = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine(B);
+                            case 2:
+                                result = A + B;
+                                Console.WriteLine(result);
                                 break;
 
                         }
